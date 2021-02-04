9d86e1fbef11e3dcb36c4f68c8d48e2e12c86c04
Mauro918 <lunatycbmx@gmail.com>
2021-02-04 09:51:40 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Empleado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Ingrese su nombre por favor ");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine(" Por favor ingrese el numero de años");
+            int antiguedad = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(" Por favor ingrese la cantidad de horas trabajadas en el mes ");
+            int cantidadhoras = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(" Por favor ingrese el valor de la hora");
+            double valorhora = double.Parse(Console.ReadLine());
+
+            int bono =  antiguedad * 50000;
+            double totalpagarhora = cantidadhoras * valorhora;
+            double totalcobrar = bono + totalpagarhora;
+            double descuento = totalcobrar * 0.15;
+            double valorneto = totalcobrar - descuento;
+
+
+            Console.WriteLine(nombre);
+            Console.WriteLine(" La cantidad de años en la enpresa es :" + antiguedad );
+            Console.WriteLine(" Valor de la hora es : + valor hora ");
+            Console.WriteLine(" Total a cobrar en bruto es: " + totalcobrar);
+            Console.WriteLine(" El descuento es : " + descuento);
+            Console.WriteLine("valor a cobrar es "); 
+
+
+
+        }
+    }
+}
